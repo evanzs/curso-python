114fca5ad1a9e6e9a309a8a2834c50df3f2a22ca
evanzs <evandrofidk@hotmail.com>
2023-09-21 09:00:01 -0300

    feat(area_circulo)::sparkles: utilizando conceito de interpretadores em python sendo alterados

---
+++ fundamentos.projetos/area-circulo-v2.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3 
+
+#utilizando shebang
+#trocando intrepetador que será usado
+# da pra executa-lo apenas no terminar sem linha de comando
+
+pi = 3.14150
+raio = 15.3
+print("Area do Circulo", pi * raio ** 2)
+
+
+
