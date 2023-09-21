b295866913c601ddb432f032a3f0135a7c9e1fe9
evanzs <evandrofidk@hotmail.com>
2023-09-21 08:59:04 -0300

    feat(area_circulo)::sparkles: utilizando um pouco de modulos

---
+++ fundamentos.projetos/area-circulo-v5.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+from math import pi
+
+raio = input('Informe o raio: ')
+
+print("Area do Circulo", pi * float(raio) ** 2)
+
+
+print("Nome do pacote",__package__)
+
+print("Nome do nome",__name__)
