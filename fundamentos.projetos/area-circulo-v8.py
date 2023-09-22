25cea4652aa16a1443505ff1ad47cb49bf9f39f4
evanzs <evandrofidk@hotmail.com>
2023-09-22 08:56:28 -0300

    feat(area-circulo):sparkles: trabalhando  com funcoes e retorno

---
+++ fundamentos.projetos/area-circulo-v8.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+from math import pi
+
+
+#trabalhando com funções em python
+
+
+def circulo(raio):
+     return pi * float(raio) ** 2
+
+
+
+if __name__ == '__main__':
+    raio = input('Informe o raio: ')
+    area = circulo(raio)
+    print(area)
