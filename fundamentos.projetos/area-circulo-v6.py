9d6448b7f96e6f372c632e699a5ccee2ed02e261
evanzs <evandrofidk@hotmail.com>
2023-09-22 08:57:09 -0300

    feat(area-circulo):sparkles: trabalhando  modulo e name modulo em python

---
+++ fundamentos.projetos/area-circulo-v6.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+from math import pi
+
+#oque define o bloco de codigo é a identacao do codigo, em python
+if __name__ == '__main__':
+    raio = input('Informe o raio: ')
+    print("Area do Circulo", pi * float(raio) ** 2)
+
+print(__name__)
