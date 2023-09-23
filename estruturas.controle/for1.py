05c29e5eaa47140432ff27b81d0f1baf9c4b711d
evanzs <evandrofidk@hotmail.com>
2023-09-23 16:10:35 -0300

    feat(for):sparkles: add estrutura de controle for

---
+++ estruturas.controle/for1.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+#Estrutura for
+#repetição dada por numero de uma estrutura ex: TUPLA
+
+
+
+#range(1,5) - define valores de 1 até 4 o ultimo valor nunca entra no range 
+for i in range(1,11): # 1 - 10
+    print('i = {}'.format(i))
