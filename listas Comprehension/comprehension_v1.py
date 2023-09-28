923257433d9696770a8e99e497ad4afa4f079a81
evanzs <evandrofidk@hotmail.com>
2023-09-27 23:03:37 -0300

    feat(comprehension_v1.py)::sparkles: trabalhando com formas diferentes de criar uma lista

---
+++ listas Comprehension/comprehension_v1.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#sintaxe
+
+
+# [ expressao for item in list if condicional]
+
+#Ex:
+
+#cria uma lista que é o dobra o valor do indice
+dobros = [ i * 2 for i in range(10)]
+print(dobros)
