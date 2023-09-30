446f92daf0efe9fbe731c7e69eca4f8f54b83139
evanzs <evandrofidk@hotmail.com>
2023-09-29 22:53:12 -0300

    feat(comprehension_v1)::sparkles: usando comprehension

--- listas Comprehension/comprehension_v1.py
+++ listas Comprehension/comprehension_v1.py
@@ -10,3 +10,12 @@
 #cria uma lista que é o dobra o valor do indice
 dobros = [ i * 2 for i in range(10)]
 print(dobros)
+
+
+
+# Versão verbosa
+
+dobros = []
+for i in range(10):
+    dobros.append(i*2)
+print(dobros)
