03d4e28f7804c4dd19f687e3ea7cd4e4de508306
evanzs <evandrofidk@hotmail.com>
2023-10-02 22:25:28 -0300

    feat(packing)::sparkles: trabalhando com packing em funcoes

---
+++ funcoes/packing.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+
+
+def soma_2(a,b):
+    return a+b
+
+def soma_3(a,b,c,d):
+    return a + b + c + d
+
+
+#empacota e cria uma tupla 
+def soma_n(*num):
+    soma = 0 
+    for n in num:
+        soma+=n
+    return soma
+
+
+##exemplo packing
+print(soma_n(2,3))
+print(soma_n(2,3,1))
+print(soma_n(1,1,1,1))
