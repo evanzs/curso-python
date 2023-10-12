5431c6b3caff6b948451fbfc389e7c55923ee04b
evanzs <evandrofidk@hotmail.com>
2023-10-12 17:45:03 -0300

    refactor(desafio_classe_caorro)::hammer: resolvendo desafio

--- poo/desafio_classe_carro.py
+++ poo/desafio_classe_carro.py
@@ -1,18 +1,29 @@
-class Data:
+#!/bin/python3
 
 
+class Carro:
+    def __init__(self,velocidade_max):
+        self.velocidade_max = velocidade_max
+        self.velocidade_atual = 0
 
+    def acelerar(self,delta=5):
+        max = self.velocidade_max
+        nova = self.velocidade_atual + delta
+        self.velocidade_atual = nova if nova <= max else max
+        return self.velocidade_atual
 
-
-
+    def frear(self,delta=5):
+        nova = self.velocidade_atual - delta
+        self.velocidade_atual = nova if nova >= 0 else 0
+        return self.velocidade_atual
 
 
 
 c1 = Carro(180) #velocidade max
 
 for _ in range(25):
-    print(c.acelerar(8))
+    print(c1.acelerar(8))
 
 
 for _ in range(10):
-    print(c.frear(delta=20))
+    print(c1.frear(delta=20))
