851d46a5eb8c4b7ff36c214c460a9eb2b036f8ee
evanzs <evandrofidk@hotmail.com>
2023-10-14 14:42:58 -0300

    refactor(funcoes_primeira_classe)::sparkles:: add another test

--- programacao_funcional/funcoes_primeira_classe.py
+++ programacao_funcional/funcoes_primeira_classe.py
@@ -14,3 +14,9 @@ funcs = [ dobro, quadrado] * 5
 for func,numero in zip(funcs,range(1,11)):
     print(f'O {func.__name__} de {numero} é {func(numero)}')
 
+
+print("\n outras funcoes: ")
+funcs = [ dobro, quadrado] 
+
+for func,numero in zip(funcs,range(1,11)):
+    print(f'O {func.__name__} de {numero} é {func(numero)}')
