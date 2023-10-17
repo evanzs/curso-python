ea5b6e2c21c68b8558e6540011d41b6da2dbe68e
evanzs <evandrofidk@hotmail.com>
2023-10-16 21:18:59 -0300

    feat(funacao_lambda_ex):sparkles: add another ex with lambda

--- programacao_funcional/funcao_lambda_ex.py
+++ programacao_funcional/funcao_lambda_ex.py
@@ -10,3 +10,13 @@ for i in a1:
     a2.append(i*2)
 
 print(a2)
+
+
+#exemplo funcional
+# uma das vantagens é não mexer o valor original "a"
+a = [1,2,3]
+m = map(lambda i: i*2,a)
+print(f'lista: {list(m)}')
+print(f'lista: {m}')
+print(f'tupla: {tuple(m)}')
+print(f'valor de a: {a}')
