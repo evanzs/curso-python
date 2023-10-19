513b2f6021aedf9328fb57965e043a5d7b0fb134
evanzs <evandrofidk@hotmail.com>
2023-10-18 21:08:04 -0300

    feat(fatorial_recursivo):sprakles: implementando fatorial de forma recursiva

--- programacao_funcional/fatorial_recursivo.py
+++ programacao_funcional/fatorial_recursivo.py
@@ -4,7 +4,9 @@ from functools import reduce
 #aplicando conceito de map
 
 def fatorial(n):
+    return ( n * fatorial(n-1)) if n > 1 else 1
 
 
 
-print(f' 10! = ' {fatorial{10}})
+
+print(f' 10! = {fatorial (10)}')
